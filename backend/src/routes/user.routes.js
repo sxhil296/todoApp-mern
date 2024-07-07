201b1a34854030220b17e2fc7562558445a71114
Sahil <leviack629@gmail.com>
2024-07-07 22:52:48 +0530

    bug fixing try

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,23 +1,22 @@
 import { Router } from "express";
 import {
-  registerUser,
   loginUser,
   logoutUser,
+  registerUser,
   refreshAccessToken,
 } from "../controllers/user.controllers.js";
+
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-//register route
 router.route("/register").post(registerUser);
 
-//login routes
 router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("refresh-token").post(refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
